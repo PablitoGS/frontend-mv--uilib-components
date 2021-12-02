e3c09cb222d38cb2e0a28b51932ce13ddcb8d5de
Pablo González <pablo.gonzalez@schibsted.com>
2021-12-02 17:13:20 +0100

    feat(components/thumbnail/picture): add Link

--- components/thumbnail/picture/src/index.js
+++ components/thumbnail/picture/src/index.js
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types'
+import {Link} from '@s-ui/react-router'
 
-export default function ThumbnailPicture({src, alt, caption}) {
+export default function ThumbnailPicture({src, alt, caption, path}) {
   return (
-    <figure className="sui-ThumbnailPicture">
-      <div className="sui-ThumbnailPicture-image">
-        <img src={src} alt={alt} />
-      </div>
-      <figcaption className="sui-ThumbnailPicture-title">{caption}</figcaption>
-    </figure>
+    <Link to={path}>
+      <figure className="sui-ThumbnailPicture">
+        <div className="sui-ThumbnailPicture-image">
+          <img src={src} alt={alt} />
+        </div>
+        <figcaption className="sui-ThumbnailPicture-title">
+          {caption}
+        </figcaption>
+      </figure>
+    </Link>
   )
 }
 
@@ -15,5 +20,6 @@ ThumbnailPicture.displayName = 'ThumbnailPicture'
 ThumbnailPicture.propTypes = {
   src: PropTypes.string,
   alt: PropTypes.string,
-  caption: PropTypes.string
+  caption: PropTypes.string,
+  path: PropTypes.string
 }
