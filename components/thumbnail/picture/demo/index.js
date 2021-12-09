de39c00cdeb24432fa72dc6c9d550df17d53ddb8
Pablo González <pablo.gonzalez@schibsted.com>
2021-12-09 17:29:44 +0100

    feat(components/thumbnail/picture/demo): demo for thumbnail picture

--- components/thumbnail/picture/demo/index.js
+++ components/thumbnail/picture/demo/index.js
@@ -1,4 +1,10 @@
 import ThumbnailPicture from 'components/thumbnail/picture/src'
 export default () => (
-  <ThumbnailPicture src="https://via.placeholder.com/150" alt="alt" />
+  <div style={{width: '400px'}}>
+    <ThumbnailPicture
+      src="https://via.placeholder.com/150"
+      alt="Image alt"
+      caption="Image placeholder"
+    />
+  </div>
 )
