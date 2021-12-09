a0ddc3865efe245ddec8876839bdd38060bb05ea
Pablo González <pablo.gonzalez@schibsted.com>
2021-12-09 17:17:25 +0100

    feat(components/thumbnail/picture): move Link to web app

--- components/thumbnail/picture/src/index.js
+++ components/thumbnail/picture/src/index.js
@@ -1,18 +1,13 @@
 import PropTypes from 'prop-types'
-import {Link} from '@s-ui/react-router'
 
-export default function ThumbnailPicture({src, alt, caption, path}) {
+export default function ThumbnailPicture({src, alt, caption}) {
   return (
-    <Link to={path}>
-      <figure className="sui-ThumbnailPicture">
-        <div className="sui-ThumbnailPicture-image">
-          <img src={src} alt={alt} />
-        </div>
-        <figcaption className="sui-ThumbnailPicture-title">
-          {caption}
-        </figcaption>
-      </figure>
-    </Link>
+    <figure className="sui-ThumbnailPicture">
+      <div className="sui-ThumbnailPicture-image">
+        <img src={src} alt={alt} />
+      </div>
+      <figcaption className="sui-ThumbnailPicture-title">{caption}</figcaption>
+    </figure>
   )
 }
 
@@ -20,6 +15,5 @@ ThumbnailPicture.displayName = 'ThumbnailPicture'
 ThumbnailPicture.propTypes = {
   src: PropTypes.string,
   alt: PropTypes.string,
-  caption: PropTypes.string,
-  path: PropTypes.string
+  caption: PropTypes.string
 }
