844347ef93f91fcf498809db34cc9a15fc4832f4
Pablo González <pablo.gonzalez@schibsted.com>
2021-12-09 10:19:56 +0100

    feat(Root): add demo for header

--- components/header/base/demo/index.js
+++ components/header/base/demo/index.js
@@ -1,2 +1,6 @@
 import HeaderBase from 'components/header/base/src'
-export default () => <HeaderBase />
+export default () => (
+  <HeaderBase>
+    <h1 style={{color: 'white'}}>Header title</h1>
+  </HeaderBase>
+)
--- components/header/search/demo/index.js
+++ components/header/search/demo/index.js
@@ -1,2 +1,25 @@
+import React, {useState} from 'react'
 import HeaderSearch from 'components/header/search/src'
-export default () => <HeaderSearch />
+
+export default () => {
+  const handleClick = () => {
+    return alert('click: ' + value)
+  }
+  const [value, setValue] = useState()
+
+  const handleChange = e => {
+    setValue(e.target.value)
+  }
+
+  return (
+    <div style={{backgroundColor: 'grey', display: 'flex'}}>
+      <HeaderSearch
+        textButton="Submit"
+        placeholder="Placeholder here"
+        value={value}
+        handleClick={handleClick}
+        handleChange={handleChange}
+      />
+    </div>
+  )
+}
