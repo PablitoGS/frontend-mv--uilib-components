6663be4c6a19733a9f5015764ae19fbcc6108e79
Pablo González <pablo.gonzalez@schibsted.com>
2021-12-10 11:55:03 +0100

    feat(components/thumbnail/list): add test for thumbnail list

--- components/thumbnail/list/test/index.test.js
+++ components/thumbnail/list/test/index.test.js
@@ -3,54 +3,29 @@
  * */
 
 /* eslint react/jsx-no-undef:0 */
-/* eslint no-undef:0 */
 
-import ReactDOM from 'react-dom'
-
+import React from 'react'
+import {render} from '@testing-library/react'
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
-import Component from '../src/index'
+import ThumbnailList from '../src/index'
 
 chai.use(chaiDOM)
 
-describe('ThumbnailList', () => {
-  const setup = setupEnvironment(Component)
+describe('ThumbnailLIst', () => {
+  it('should display a list', () => {
+    const {getByText} = render(
+      <ThumbnailList>
+        <ThumbnailList.Item>Item 1</ThumbnailList.Item>
+        <ThumbnailList.Item>Item 2</ThumbnailList.Item>
+        <ThumbnailList.Item>Item 3</ThumbnailList.Item>
+        <ThumbnailList.Item>Item 4</ThumbnailList.Item>
+      </ThumbnailList>
+    )
 
-  it('should render without crashing', () => {
-    // Given
-    const props = {}
-
-    // When
-    const component = <Component {...props} />
-
-    // Then
-    const div = document.createElement('div')
-    ReactDOM.render(component, div)
-    ReactDOM.unmountComponentAtNode(div)
-  })
-
-  it('should NOT render null', () => {
-    // Given
-    const props = {}
-
-    // When
-    const {container} = setup(props)
-
-    // Then
-    expect(container.innerHTML).to.be.a('string')
-    expect(container.innerHTML).to.not.have.lengthOf(0)
-  })
-
-  it.skip('should NOT extend classNames', () => {
-    // Given
-    const props = {className: 'extended-classNames'}
-    const findSentence = str => string => string.match(new RegExp(`S*${str}S*`))
-
-    // When
-    const {container} = setup(props)
-    const findClassName = findSentence(props.className)
-
-    // Then
-    expect(findClassName(container.innerHTML)).to.be.null
+    expect(getByText('Item 1')).to.be.visible
+    expect(getByText('Item 2')).to.be.visible
+    expect(getByText('Item 3')).to.be.visible
+    expect(getByText('Item 4')).to.be.visible
   })
 })
