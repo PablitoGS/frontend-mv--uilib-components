510c7712888abfa1912f82f4923394ac33f06188
Pablo González <pablo.gonzalez@schibsted.com>
2021-12-10 11:21:13 +0100

    feat(Root): add test for header

--- components/header/base/test/index.test.js
+++ components/header/base/test/index.test.js
@@ -5,52 +5,22 @@
 /* eslint react/jsx-no-undef:0 */
 /* eslint no-undef:0 */
 
-import ReactDOM from 'react-dom'
-
+import React from 'react'
+import {render} from '@testing-library/react'
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
-import Component from '../src/index'
+import HeaderBase from '../src/index'
 
 chai.use(chaiDOM)
 
 describe('HeaderBase', () => {
-  const setup = setupEnvironment(Component)
+  it('should display header', () => {
+    const {getByText} = render(
+      <HeaderBase>
+        <header>Heading</header>
+      </HeaderBase>
+    )
 
-  it('should render without crashing', () => {
-    // Given
-    const props = {}
-
-    // When
-    const component = <Component {...props} />
-
-    // Then
-    const div = document.createElement('div')
-    ReactDOM.render(component, div)
-    ReactDOM.unmountComponentAtNode(div)
-  })
-
-  it('should NOT render null', () => {
-    // Given
-    const props = {}
-
-    // When
-    const {container} = setup(props)
-
-    // Then
-    expect(container.innerHTML).to.be.a('string')
-    expect(container.innerHTML).to.not.have.lengthOf(0)
-  })
-
-  it.skip('should NOT extend classNames', () => {
-    // Given
-    const props = {className: 'extended-classNames'}
-    const findSentence = str => string => string.match(new RegExp(`S*${str}S*`))
-
-    // When
-    const {container} = setup(props)
-    const findClassName = findSentence(props.className)
-
-    // Then
-    expect(findClassName(container.innerHTML)).to.be.null
+    expect(getByText('Heading')).to.be.visible
   })
 })
